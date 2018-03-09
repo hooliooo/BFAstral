2aa1e3052c968def0e342dfa3acfbb3de62de580
hooliooo <alorro3@gmail.com>
2018-03-09 20:57:21 +0800

    Initial Commit

---
+++ BFAstral/BFAstral.h
@@ -0,0 +1,19 @@
+//
+//  BFAstral.h
+//  BFAstral
+//
+//  Created by Julio Miguel Alorro on 3/9/18.
+//  Copyright © 2018 Julio Alorro Software Development, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BFAstral.
+FOUNDATION_EXPORT double BFAstralVersionNumber;
+
+//! Project version string for BFAstral.
+FOUNDATION_EXPORT const unsigned char BFAstralVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BFAstral/PublicHeader.h>
+
+
